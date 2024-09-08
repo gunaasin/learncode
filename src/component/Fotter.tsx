92b5a65ef7f84c684a07bbee58fdcf2136b1dc04
gunaasin <guna.asin06@gmail.com>
2024-09-08 22:50:10 +0530

    next-v

--- src/component/Fotter.tsx
+++ src/component/Fotter.tsx
@@ -8,7 +8,7 @@ export const Fotter = () => {
    <div className="info">
     <h4>design and develop</h4>
     <div className="infoid">
-    <a href="">James  k.M</a>&nbsp;|&nbsp;<a href="">Dinesh  t</a>&nbsp;|&nbsp;<a href="">Guna  M</a>
+    <a href="https://gunaportfoliogn.vercel.app/">Guna  M</a> &nbsp;|&nbsp;<a href="">James  k.M</a>&nbsp;|&nbsp;<a href="">Dinesh  t</a>
     </div> 
     
   
